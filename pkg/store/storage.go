7eec08fbcb65c11a18fdda4d2fdecea48b41c500
ichandxyx <chand99kumar@gmail.com>
2021-05-19 14:12:24 +0530

    initial commit

--- pkg/store/storage.go
+++ pkg/store/storage.go
@@ -1,1 +1,15 @@
 package store
+
+import (
+	"entgo.io/ent/entc/integration/multischema/ent"
+)
+
+type Storage struct {
+	db *ent.Client
+}
+
+func New(client *ent.Client) *Storage{
+	return &Storage{
+		db:client,
+	}
+}
