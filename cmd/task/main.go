2ba64d933a788f8cb23de219116404c850c4aa2f
ichandxyx <chand99kumar@gmail.com>
2021-05-19 14:00:28 +0530

    basic setup

---
+++ cmd/task/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+
+	"entgo.io/ent/entc/integration/multischema/ent"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-kit/kit/log"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/go-kit/kit/log/level"
+	"github.com/ichandxyx/task/pkg/store"
+)
+
+func main() {
+
+	logger :=log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
+	logger=log.With(logger,"ts",log.DefaultTimestampUTC,"caller",log.DefaultCaller)
+
+	client,err:=ent.Open("sqllite3","file:"+"task.db"+"?cache=shared&mode=rwc&_fk=1")
+	if err !=nil{
+		level.Error(logger).Log("err",err)
+
+	}
+	level.Info(logger).Log("msg", "successfully connected to database")
+	st:=store.New(client)
+	ap:=api.New(st,log.With(logger,"component","api"))
+	r:=chi.NewRouter()
+	r.Route("/api",func(r chi.Router){
+		ap.Register(r)
+	})
+
+	level.Info(logger).Log("msg", "starting web server", "addr", "0.0.0.0:8080")
+	if err := http.ListenAndServe("0.0.0.0:8080", r); err != nil && err != http.ErrServerClosed {
+		level.Error(logger).Log("msg", "http server listen", "err", err)
+	}
+
+
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/ichandxyx/task
+
+go 1.16
+
+require (
+	entgo.io/ent v0.8.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
+	github.com/go-kit/kit v0.10.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
+)
