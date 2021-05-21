279dc06796d11a3ba91645f40124c52b586f9a28
ichandxyx <chand99kumar@gmail.com>
2021-05-21 14:39:42 +0530

    error removed in image unknown format

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,15 +1,21 @@
+ 
 package api
 
 import (
 	"context"
+	
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 	"github.com/ichandxyx/task/ent"
 
 	//"github.com/ichandxyx/task/ent/visit"
-	"github.com/ichandxyx/task/pkg/img"
+	//"github.com/ichandxyx/task/pkg/img"
 	"github.com/ichandxyx/task/pkg/store"
+	"image"
+    _ "image/jpeg"
+    "net/http"
 )
 
 type API struct {
@@ -36,11 +42,19 @@ func (a *API) calculatePerimeter(ctx context.Context, jobid int, visits []*ent.V
 		var p int
 		//a.store.UpdatePerimeter(ctx,&(vi.ImageURLs))
 		for _, i := range vi.ImageURLs {
-			config, err := img.ImageConfigFromURL(i)
+			resp, err := http.Get(i)
+			if err!=nil{
+				errFlag = true
+			}
+			defer resp.Body.Close()
+			config, _, err:=image.DecodeConfig(resp.Body)
+			//config, err := img.ImageConfigFromURL(i)
 			if err != nil {
 				// perimeters[vi.StoreID]=-1
 				// errs[vi.StoreID]="image download failed"
 				errFlag = true
+					//ahndle errer
+					level.Error(a.logger).Log("err", err)
 				break
 			}
 			p += 2 * (config.Height + config.Width)
